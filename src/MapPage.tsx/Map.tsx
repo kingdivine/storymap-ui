aa772dccac3a2f2bb30931d228b0a68f2e4f2466
kingdivine <divineanamekwe@gmail.com>
2021-04-02 23:25:06 +0100

    Add markers to map using dummy geojson data.

--- src/MapPage.tsx/Map.tsx
+++ src/MapPage.tsx/Map.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import { makeStyles } from "@material-ui/core/styles";
+import dummyData from "./data.json";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZGl2aW5lYSIsImEiOiJja24wZ2lqbjkwY2J4Mm9scnY3bW1yZW5nIn0.GkVgq5TQlU19vuZLwggtjQ";
 const MAP_THEME = "mapbox://styles/mapbox/dark-v10";
+
+const mock: any = dummyData;
 
 const useStyles = makeStyles({
   mapContainer: {
@@ -31,8 +34,8 @@ const useStyles = makeStyles({
 export default function Map() {
   const classes = useStyles();
   const mapContainer = useRef(null);
-  const [long, setLong] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
+  const [long, setLong] = useState(70.048828125);
+  const [lat, setLat] = useState(44.809121700077355);
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
@@ -49,6 +52,44 @@ export default function Map() {
       setLong(newLong);
       setLat(newLat);
       setZoom(newZoom);
+    });
+
+    map.on("load", function () {
+      // Add an image to use as a custom marker
+      map.loadImage(
+        "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+        function (error, image) {
+          if (error) throw error;
+          map.addImage("custom-marker", image!);
+          // Add a GeoJSON source
+          map.addSource("posts", {
+            type: "geojson",
+            data: mock,
+            cluster: true,
+            clusterMaxZoom: 14,
+            clusterRadius: 50,
+          });
+
+          // Add a symbol layer
+          map.addLayer({
+            id: "posts",
+            type: "symbol",
+            source: "posts",
+            layout: {
+              "icon-image": "custom-marker",
+              // get the title name from the source's "title" property
+              "text-field": ["get", "title"],
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 1.5],
+              "text-anchor": "top",
+              "text-size": 12,
+            },
+            paint: {
+              "text-color": "white",
+            },
+          });
+        }
+      );
     });
     return () => map.remove();
     // eslint-disable-next-line
