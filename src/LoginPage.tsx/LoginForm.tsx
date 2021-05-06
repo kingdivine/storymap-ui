c522f558eedf3000f2058e0dff8bab264176a9d2
kingdivine <divineanamekwe@gmail.com>
2021-05-06 22:47:02 +0100

    Add form state.

--- src/LoginPage.tsx/LoginForm.tsx
+++ src/LoginPage.tsx/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
@@ -52,17 +52,58 @@ function TabPanel(props: TabPanelProps) {
 
 export default function LoginForm() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [tabValue, setTabValue] = useState(0);
 
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setValue(newValue);
+  const [loginValues, setLoginValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [signUpValues, setSignUpValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTabValue(newValue);
+  };
+
+  const handleLoginTextFieldChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    field: "email" | "password"
+  ) => {
+    setLoginValues({
+      ...loginValues,
+      [field]: e.target.value,
+    });
+  };
+
+  const handleSignUpTextFieldChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    field: "username" | "email" | "password" | "confirmPassword"
+  ) => {
+    setSignUpValues({
+      ...signUpValues,
+      [field]: e.target.value,
+    });
+  };
+
+  const canSignUp = () =>
+    isMatchingPassword() &&
+    Object.values(signUpValues).every((value) => value.length > 0);
+
+  const isMatchingPassword = () => {
+    if (signUpValues.password.length > 0) {
+      return signUpValues.confirmPassword === signUpValues.password;
+    }
+    return true;
   };
 
   return (
     <Paper className={classes.root}>
       <Tabs
-        value={value}
-        onChange={handleChange}
+        value={tabValue}
+        onChange={handleTabChange}
         indicatorColor="primary"
         textColor="primary"
         centered
@@ -70,12 +111,14 @@ export default function LoginForm() {
         <Tab label="Login" />
         <Tab label="Sign Up" />
       </Tabs>
-      <TabPanel value={value} index={0}>
+      <TabPanel value={tabValue} index={0}>
         <div className={classes.formContainer}>
           <TextField
             placeholder="email"
             variant="outlined"
             size="small"
+            value={loginValues.email}
+            onChange={(e) => handleLoginTextFieldChange(e, "email")}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -88,6 +131,8 @@ export default function LoginForm() {
             placeholder="password"
             variant="outlined"
             size="small"
+            value={loginValues.password}
+            onChange={(e) => handleLoginTextFieldChange(e, "password")}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -103,12 +148,14 @@ export default function LoginForm() {
           </Button>
         </div>
       </TabPanel>
-      <TabPanel value={value} index={1}>
+      <TabPanel value={tabValue} index={1}>
         <div className={classes.formContainer}>
           <TextField
             placeholder="username"
             variant="outlined"
             size="small"
+            value={signUpValues.username}
+            onChange={(e) => handleSignUpTextFieldChange(e, "username")}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -121,6 +168,8 @@ export default function LoginForm() {
             placeholder="email"
             variant="outlined"
             size="small"
+            value={signUpValues.email}
+            onChange={(e) => handleSignUpTextFieldChange(e, "email")}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -133,6 +182,8 @@ export default function LoginForm() {
             placeholder="password"
             variant="outlined"
             size="small"
+            value={signUpValues.password}
+            onChange={(e) => handleSignUpTextFieldChange(e, "password")}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -145,6 +196,10 @@ export default function LoginForm() {
             placeholder="confirm password"
             variant="outlined"
             size="small"
+            error={!isMatchingPassword()}
+            helperText={!isMatchingPassword() ? "Passwords must match" : null}
+            value={signUpValues.confirmPassword}
+            onChange={(e) => handleSignUpTextFieldChange(e, "confirmPassword")}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -155,7 +210,12 @@ export default function LoginForm() {
           />
         </div>
         <div className={classes.btnContainer}>
-          <Button size="small" variant={"contained"} color={"primary"}>
+          <Button
+            size="small"
+            variant={"contained"}
+            color={"primary"}
+            disabled={!canSignUp()}
+          >
             Sign Up
           </Button>
         </div>
