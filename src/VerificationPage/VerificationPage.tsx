1c771b28c83c5575fc40dcbc4ccb056f2a19045b
kingdivine <divineanamekwe@gmail.com>
2021-09-12 21:21:01 +0100

    Post verification code to server. Display confirmation messages.

--- src/VerificationPage/VerificationPage.tsx
+++ src/VerificationPage/VerificationPage.tsx
@@ -1,7 +1,20 @@
-import { createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
+import { useRef, useState } from "react";
+import {
+  Button,
+  CircularProgress,
+  createStyles,
+  makeStyles,
+  Paper,
+  Theme,
+  Typography,
+} from "@material-ui/core";
 import Heading from "../Generic/Heading";
 import mapBackground from "../Generic/images/map-background.png";
 import Footer from "../Generic/Footer";
+import { useHistory } from "react-router-dom";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { useEffect } from "react";
+import axios from "axios";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,11 +37,51 @@ const useStyles = makeStyles((theme: Theme) =>
       alignItems: "center",
       flexDirection: "column",
     },
+    msgBox: {
+      padding: theme.spacing(2),
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+    },
   })
 );
 
 export default function VerificationPage() {
   const classes = useStyles();
+  let history = useHistory();
+  const hasFetchedData = useRef(false);
+  const [currentUser, setCurrentUser] = useLocalStorage("currentUser", null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    if (hasFetchedData.current) {
+      return;
+    }
+    hasFetchedData.current = true;
+    setIsLoading(true);
+    setErrorMsg("");
+    axios
+      .get(`/storymap-api${history.location.pathname}`)
+      .then((response) => {
+        const userObj = { ...response.data.user, token: response.data.token };
+        setCurrentUser(userObj);
+      })
+      .catch((e) => {
+        if (e.response && e.response.status === 401) {
+          setErrorMsg("Code not recognised or user already verified.");
+        } else {
+          setErrorMsg(
+            "Please check your internet connection and try again later."
+          );
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [history, setCurrentUser]);
+
   return (
     <div className={classes.pageContainer}>
       <header className={classes.header}>
@@ -38,6 +91,46 @@ export default function VerificationPage() {
       </header>
       <div className={classes.verificationContainer}>
         <Heading />
+        <Paper className={classes.msgBox}>
+          {isLoading && (
+            <>
+              <Typography variant="h6">
+                Just a sec, verifying account...
+              </Typography>
+              <CircularProgress style={{ margin: 16 }} color="secondary" />
+            </>
+          )}
+          {!isLoading && !errorMsg && currentUser && (
+            <>
+              <Typography variant="h4">
+                Hey, {currentUser?.username}!
+              </Typography>
+              <br />
+              <Typography variant="h6">
+                Your email verification was successful.
+              </Typography>
+              <Typography variant="h6"> Welcome to Storymap.</Typography>
+              <Button
+                size="small"
+                variant={"contained"}
+                color={"primary"}
+                onClick={() => history.push("/")}
+                style={{ margin: 8 }}
+              >
+                Tell your story
+              </Button>
+            </>
+          )}
+          {!isLoading && errorMsg && (
+            <>
+              <Typography variant="h4" color="secondary">
+                Oops! Something went wrong.
+              </Typography>
+              <br />
+              <Typography variant="h6">{errorMsg}</Typography>
+            </>
+          )}
+        </Paper>
       </div>
       <Footer />
     </div>
