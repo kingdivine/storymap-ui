184cbfe2881c36f2356f67ead18da815f3e5cb8c
kingdivine <divineanamekwe@gmail.com>
2021-10-28 22:36:29 +0100

    Add cluster click code to map. Create and show posts in dialog.

---
+++ src/MapPage.tsx/StoryClusterDialog.tsx
@@ -0,0 +1,75 @@
+import {
+  Dialog,
+  Typography,
+  IconButton,
+  createStyles,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import { useHistory } from "react-router";
+import UsernameAndPic from "../Generic/UsernameandPic";
+import { Story } from "../types/Story";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    topLineContainer: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      margin: theme.spacing(1),
+    },
+    listItem: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "start",
+      marginBottom: theme.spacing(1),
+    },
+  })
+);
+
+export default function StoryClusterDialog(props: {
+  posts: Story[];
+  onCloseDialog: () => void;
+}) {
+  const { posts, onCloseDialog } = props;
+  const classes = useStyles();
+  let history = useHistory();
+
+  return (
+    <Dialog onClose={() => onCloseDialog()} open={true}>
+      <div style={{ margin: 8, padding: 16 }}>
+        <div className={classes.topLineContainer}>
+          <div />
+          <Typography style={{ width: "50%" }} color={"textSecondary"}>
+            {`${posts.length} Stories`}
+          </Typography>
+          <IconButton onClick={() => onCloseDialog()}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div>
+          {posts.map((post) => (
+            <div className={classes.listItem} key={post.id}>
+              <UsernameAndPic
+                userId={post.author_id}
+                username={post.author_username}
+                avatar={post.author_avatar}
+                small
+              />
+
+              <Typography
+                variant={"body1"}
+                style={{ marginLeft: 38, cursor: "pointer" }}
+                onClick={() => history.push(`/story/${post.slug}`)}
+              >
+                {post.title}
+              </Typography>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Dialog>
+  );
+}
