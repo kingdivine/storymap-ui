450da85e89765db911a38a124989781a7822a878
kingdivine <divineanamekwe@gmail.com>
2021-11-07 17:37:11 +0000

    Move 'about' nav link on login page.

--- src/Generic/Navbar.tsx
+++ src/Generic/Navbar.tsx
@@ -1,39 +1,16 @@
 import { useState, useEffect } from "react";
 import { useCurrentUser } from "../hooks/useCurrentUser";
 import axios from "axios";
-import { createStyles, Theme, Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import { NotificationCounts } from "../types/NotificationCounts";
 import Heading from "./Heading";
-import { useHistory } from "react-router";
 import BottomBar from "./BottomBar";
 import TopBar from "./TopBar";
 import { isMobile } from "../utils";
 
 const smallScreen = isMobile();
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    navBar: {
-      position: "fixed",
-      right: theme.spacing(5),
-      top: theme.spacing(4),
-      zIndex: 1,
-      display: "flex",
-      flexDirection: "row",
-    },
-    navLinkBtn: {
-      textTransform: "lowercase",
-      color: theme.palette.common.white,
-    },
-  })
-);
-
 export default function Navbar(props: { fetchNotifs: boolean }) {
-  const classes = useStyles();
-
   const [currentUser] = useCurrentUser();
-  const history = useHistory();
 
   const [unreadNotifsCount, setUnreadNotifsCount] = useState(0);
 
@@ -50,19 +27,6 @@ export default function Navbar(props: { fetchNotifs: boolean }) {
       .then((response) => setUnreadNotifsCount(response.data.un_read))
       .catch((e) => console.log(e));
   }, [currentUser, props.fetchNotifs]);
-
-  if (history.location.pathname === "/login") {
-    return (
-      <>
-        <Heading />
-        <header className={classes.navBar}>
-          <Button href="/about" style={{ textTransform: "none" }}>
-            What is Storymap?
-          </Button>
-        </header>
-      </>
-    );
-  }
 
   if (smallScreen) {
     return (
