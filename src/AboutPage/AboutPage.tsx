12d599ec77b643f1faa9b742fe4d5aa0bfa1c024
kingdivine <divineanamekwe@gmail.com>
2021-11-07 18:10:34 +0000

    Jazz up about page - add emojis and color.

--- src/AboutPage/AboutPage.tsx
+++ src/AboutPage/AboutPage.tsx
@@ -1,7 +1,6 @@
 import {
   Button,
   createStyles,
-  Divider,
   makeStyles,
   Paper,
   Theme,
@@ -9,14 +8,17 @@ import {
 } from "@material-ui/core";
 import Heading from "../Generic/Heading";
 import mapBackground from "../Generic/images/map-background.png";
+import { isMobile } from "../utils";
 
 const PARAGRAPHS = [
-  "Storymap is an online travel diary. A place to share your stories and experiences from your community and around the world.",
-  "It’s a place to journal your adventures and encounters, your insights and your life.",
-  "Document stories for your own private viewing - as a memento for your future self - or post in public mode, for all to see and share.",
-  "Simply curious? Well, fly around the map to various locations, and check out what’s happening over here and over there.",
+  "Storymap is an online travel diary. A place to share your stories and experiences from your community and around the world. It’s a place to journal your adventures and encounters, your insights and your life.",
+  "Document stories for your own private viewing - as a memento for your future self - or post in public mode, for all to see and share. Simply curious? Well, fly around the map to various locations, and check out what’s happening over here and over there.",
   "There’s 8 billion stories to be written. Why not write yours?",
 ];
+
+const DIVIDER_EMOJIS = [0x1f58b, 0x1f30e, 0x1f918];
+
+const smallScreen = isMobile();
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -36,7 +38,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     paper: {
       padding: theme.spacing(2),
-      maxWidth: "85%",
+      maxWidth: smallScreen ? "85%" : "60%",
       textAlign: "center",
       "&>*": {
         margin: theme.spacing(2),
@@ -54,10 +56,14 @@ export default function AboutPage() {
       <Heading />
       <div className={classes.paperContainer}>
         <Paper className={classes.paper}>
-          <Typography variant={"h5"}>What is Storymap?</Typography>
-          <Divider />
-          {PARAGRAPHS.map((p) => (
-            <Typography variant={"body1"}>{p}</Typography>
+          <Typography variant={"h5"} color="secondary">
+            What is Storymap?
+          </Typography>
+          {PARAGRAPHS.map((p, i) => (
+            <>
+              <Typography variant={"body1"}>{p}</Typography>
+              <div>{String.fromCodePoint(DIVIDER_EMOJIS[i])}</div>
+            </>
           ))}
           <Button
             variant="contained"
