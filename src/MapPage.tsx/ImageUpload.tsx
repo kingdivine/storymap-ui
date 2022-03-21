58a8d0a388bdef65f19d57fb77c969fa3a83a458
kingdivine <divineanamekwe@gmail.com>
2022-03-21 18:34:25 +0000

    Limit no of images. Add remove btn.

--- src/MapPage.tsx/ImageUpload.tsx
+++ src/MapPage.tsx/ImageUpload.tsx
@@ -5,19 +5,27 @@ import {
   createStyles,
   Typography,
   Button,
+  IconButton,
 } from "@material-ui/core";
 import { isMobile } from "../utils";
-import { PhotoCamera } from "@material-ui/icons";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import RemoveIcon from "@material-ui/icons/Clear";
 
 const smallScreen = isMobile();
 const THUMBNAIL_SIZE = smallScreen ? 75 : 100;
 const FILE_SIZE_LIMIT = 5000000; //5MB
+const MAX_IMAGES = 3;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     thumbnailContainer: {
       display: "flex",
       flexDirection: "row",
+    },
+    thumbnailAndRemoveBtn: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
     },
     thumbnail: {
       width: `${THUMBNAIL_SIZE}px`,
@@ -29,22 +37,33 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundPosition: "center center",
       backgroundSize: "cover",
     },
+    removeBtn: {
+      height: "3px",
+      width: "3px",
+      marginTop: "3px",
+      color: theme.palette.secondary.main,
+    },
   })
 );
 
 export default function ImageUpload() {
   const [imageFiles, setImageFiles] = useState<File[]>([]);
-  const [imageObjectUrls, setImageObjectUrls] = useState([""]);
+  const [imageObjectUrls, setImageObjectUrls] = useState<
+    { name: string; url: string }[]
+  >([]);
   const [errorText, setErrorText] = useState("");
 
   const classes = useStyles();
 
   useEffect(() => {
-    const urlsToSet = imageFiles.map((image) => URL.createObjectURL(image));
+    const urlsToSet = imageFiles.map((image) => ({
+      name: image.name,
+      url: URL.createObjectURL(image),
+    }));
     setImageObjectUrls(urlsToSet);
   }, [imageFiles]);
 
-  const onImagesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     setErrorText("");
     const files = e.target.files as FileList;
     const fileArr = Array.from(files).map((file) => file);
@@ -58,6 +77,13 @@ export default function ImageUpload() {
     }
   };
 
+  const handleRemoveClick = (imageName: string) => {
+    const updatedImageFiles = imageFiles.filter(
+      (image) => image.name !== imageName
+    );
+    setImageFiles(updatedImageFiles);
+  };
+
   return (
     <>
       <div>
@@ -67,6 +93,7 @@ export default function ImageUpload() {
             size="small"
             component="span"
             startIcon={<PhotoCamera />}
+            disabled={imageFiles.length === MAX_IMAGES}
           >
             Add Images
           </Button>
@@ -77,24 +104,42 @@ export default function ImageUpload() {
           style={{ display: "none" }}
           multiple
           accept="image/*"
-          onChange={onImagesChange}
+          onChange={onImageUpload}
+          disabled={imageFiles.length === MAX_IMAGES}
         />
       </div>
 
       <div className={classes.thumbnailContainer}>
-        {imageObjectUrls.map((url) => (
-          <div
-            className={classes.thumbnail}
-            style={{
-              backgroundImage: `url('${url}')`,
-            }}
-            title={url}
-          ></div>
+        {imageObjectUrls.map((image) => (
+          <div className={classes.thumbnailAndRemoveBtn}>
+            <div
+              className={classes.thumbnail}
+              style={{
+                backgroundImage: `url('${image.url}')`,
+              }}
+              title={image.url}
+            />
+
+            <IconButton
+              className={classes.removeBtn}
+              onClick={() => handleRemoveClick(image.name)}
+            >
+              <RemoveIcon />
+            </IconButton>
+          </div>
         ))}
       </div>
-      <Typography variant="caption" color="error">
-        {errorText}
-      </Typography>
+      {errorText && (
+        <Typography variant="caption" color="error">
+          {errorText}
+        </Typography>
+      )}
+
+      {!errorText && imageFiles.length === MAX_IMAGES && (
+        <Typography variant="caption" color="textPrimary">
+          Max of 3 images added.
+        </Typography>
+      )}
     </>
   );
 }
