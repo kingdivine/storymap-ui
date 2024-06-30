7bcad26b5a6739172cb6ded022141c404b1aa5f4
kingdivine <divineanamekwe@gmail.com>
2024-06-30 17:53:46 +0100

    Add eslint.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   base: "",
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    eslint({ exclude: ["node_modules", "dist", "build", "public"] }),
+  ],
   server: {
     open: true,
     port: 3000,
